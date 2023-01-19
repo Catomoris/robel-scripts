ea6fe21f92fdc847b15bb3000b63589c9c61d08a
Catomoris <122347492+Catomoris@users.noreply.github.com>
2023-01-19 16:23:56 -0500

    bobel scripts

---
+++ config.py
@@ -0,0 +1,12 @@
+# place any of your own overrides here225944
+# see bombsquad_server for details on what you can override
+# examples (uncomment to use):
+config['partyName'] = 'CAOS SUPER SMASH | FFA'
+config['sessionType'] = 'ffa'
+config['maxPartySize'] = 14
+config['port'] = 43210
+config['playlistCode'] = 404383
+config['statsURL'] = '20.25.38.131'
+config['enableTelnet'] = True
+config['telnetPassword'] = 'broody'
+config['telnetPort'] = 43210
---
+++ data/scripts/getPermissionsHashes.py
@@ -0,0 +1,22 @@
+adminHashes = ['pb-IF4zV2JdMw==', 'pb-IF4yV04-Uw==', 'pb-IF4xVBcjFw==']
+vipHashes = ['pb-IF4gVGIeDQ==', 'pb-IF4RU1YCMw==']
+banlist = ['']
+topperslist = []
+effectCustomers = {}
+customlist = {'pb-IF4yV04-Uw==': ''}
+ownerHashes = ['pb-IF4zV2JdMw==', 'pb-IF4yV04-Uw==']
+surroundingObjectEffect = []
+sparkEffect = ['pb-IF4xVBcjFw==']
+smokeEffect = []
+scorchEffect = [] 
+distortionEffect = []
+glowEffect = ['pb-IF4yV04-Uw=='] 
+iceEffect=['pb-IF4zV2JdMw==']
+slimeEffect = ['pb-IF4yV04-Uw==']
+metalEffect = []
+dragonHashes = []
+customtagHashes=[]
+
+#donot change the order of the list
+#to enable/disable commands and effects for top 5 players goto settings.py
+
